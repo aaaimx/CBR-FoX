7bb87956aac16fd198ad1da056ee49cfb8df8fa3
Gerardo Pérez Pérez <jerryperezxbox@gmail.com>
2025-08-29 12:45:16 -0600

    Se actualiza el setup.py para la correción en el workflow de publicación a pypi

--- setup.py
+++ setup.py
@@ -1,9 +1,18 @@
 from setuptools import setup, find_packages
+from pathlib import Path
+
+def parse_requirements(filename):
+    return [
+        line.strip()
+        for line in Path(filename).read_text().splitlines()
+        if line.strip() and not line.startswith("#")
+    ]
 
 setup(
     name="CBR-FoX",
-    version="1.0.0",
-    long_description=open("README.md").read(),    # Root level
-    install_requires=open("requirements.txt"),    # Root level
-    packages=find_packages(),                     # Scans from root
+    version="0.3.0",
+    long_description=Path("README.md").read_text(encoding="utf-8"),
+    long_description_content_type="text/markdown",
+    install_requires=parse_requirements("requirements.txt"),
+    packages=find_packages(),
 )
